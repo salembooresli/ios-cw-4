5855f810d3749c2063e8b2783718028d7fa2bcee
salembooresli <70517601+salembooresli@users.noreply.github.com>
2020-09-24 23:09:25 +0300

    Done!!

---
+++ ios-cw-4-part2.playground/Contents.swift
@@ -0,0 +1,18 @@
+struct Language {
+    var hello:String
+    var flag:String
+    func greeting(hello:String,flag:String) -> String {
+        return "\(hello)\(flag)"
+    }
+}
+var language = [
+    Language(hello: "kuwait", flag: "🇰🇼"),
+  Language(hello: "spain" , flag: "🇪🇸")
+]
+Language(hello: "kuwait", flag:"🇰🇼" )
+for _ in language {
+    for _ in language {
+        language
+    }
+}
+
